b7097f2286ed0d586fbd99e651959fd3198c759c
William Byrd <webyrd@gmail.com>
2019-05-07 11:53:41 -0500

    added test

--- templates/template-tests.rkt
+++ templates/template-tests.rkt
@@ -15,6 +15,14 @@
           (,C (car l) (reverse (cdr l)))))))
 
 (check-equal?
+ (const-pattern 'reverse (list (cons (list (list 1 #f)) (list #f 1))))
+ '(define reverse
+    (lambda (l)
+      (if (null? l)
+          ,B
+          (,C (car l) (reverse (cdr l)))))))
+
+(check-equal?
  (const-pattern 'reverse '((((1 2)) . (2 1))))
  '(define reverse
     (lambda (l)
