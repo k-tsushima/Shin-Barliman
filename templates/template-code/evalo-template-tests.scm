326a936e4994c1f8d4b0ac5ecc0b4c0dd65df178
William Byrd <webyrd@gmail.com>
2019-06-02 21:31:51 -0500

    added another evalo example

--- templates/template-code/evalo-template-tests.scm
+++ templates/template-code/evalo-template-tests.scm
@@ -1,6 +1,94 @@
 (check-equal?
- (list 1 2 3)
- '(1 2 3))
+ (let ()
+       (define (ans-allTests)
+         (define (results)
+           (run 1 (defns)
+             (let ((g1 (gensym "g1"))
+                   (g2 (gensym "g2"))
+                   (g3 (gensym "g3"))
+                   (g4 (gensym "g4"))
+                   (g5 (gensym "g5"))
+                   (g6 (gensym "g6"))
+                   (g7 (gensym "g7"))
+                   (g8 (gensym "g8"))
+                   (g9 (gensym "g9"))
+                   (g10 (gensym "g10"))
+                   (g11 (gensym "g11"))
+                   (g12 (gensym "g12"))
+                   (g13 (gensym "g13"))
+                   (g14 (gensym "g14"))
+                   (g15 (gensym "g15"))
+                   (g16 (gensym "g16"))
+                   (g17 (gensym "g17"))
+                   (g18 (gensym "g18"))
+                   (g19 (gensym "g19"))
+                   (g20 (gensym "g20")))
+               (fresh (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z begin-body)
+                 (fresh (defn-list)
+               
+                   (== defns defn-list)
+               
+                   (absento g1 defn-list)
+                   (absento g2 defn-list)
+                   (absento g3 defn-list)
+                   (absento g4 defn-list)
+                   (absento g4 defn-list)
+                   (absento g5 defn-list)
+                   (absento g6 defn-list)
+                   (absento g7 defn-list)
+                   (absento g8 defn-list)
+                   (absento g9 defn-list)
+                   (absento g10 defn-list)
+                   (absento g11 defn-list)
+                   (absento g12 defn-list)
+                   (absento g13 defn-list)
+                   (absento g14 defn-list)
+                   (absento g15 defn-list)
+                   (absento g16 defn-list)
+                   (absento g17 defn-list)
+                   (absento g18 defn-list)
+                   (absento g19 defn-list)
+                   (absento g20 defn-list))
+
+                 ;; skeleton
+                 (== `((define double
+                         (lambda (l)
+                           (match l
+                             (`() '())
+                             (`(,a . ,d)
+                              (cons ,C (double d)))))))
+                     defns)
+
+                 (appendo defns
+                          `(((lambda x x)
+
+                             ;; example inputs
+                             (double '())
+                             (double '(1))
+                             (double '(2 1))
+                             (double '(2 3 4 1))
+
+                             ))
+                          begin-body)
+                 (evalo `(begin . ,begin-body)
+                        (list
+                         
+                         ;; example outputs
+                         '()
+                         '((1 1))
+                         '((2 2) (1 1))
+                         '((2 2) (3 3) (4 4) (1 1))
+                         ))
+                 ))))
+         (results))
+
+       (ans-allTests))
+ '((((define double
+       (lambda (l)
+         (match l
+           (`() '())
+           (`(,a unquote d)
+            (cons (list a a) (double d))))))))))
 
 (check-equal?
  (let ()
