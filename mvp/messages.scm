c3b910edf40515b055d024a859874e8cb453f215
William Byrd <webyrd@gmail.com>
2019-12-18 19:02:09 +0900

    added list of messages passed between the parts of Shin-Barliman

---
+++ mvp/messages.scm
@@ -0,0 +1,83 @@
+#|
+
+Message types sent and received from UI, MCP, SCP, Synthesis subprocesses
+
+|#
+
+;===================
+UI
+;===================
+
+;--------------------
+Received from MCP
+;--------------------
+
+;--------------------
+Sent to MCP
+;--------------------
+
+
+
+;===================
+MCP
+;===================
+
+;--------------------
+Received from UI
+;--------------------
+
+;--------------------
+Sent to UI
+;--------------------
+
+;--------------------
+Received from SCP
+;--------------------
+
+;--------------------
+Sent to SCP
+;--------------------
+
+
+
+;===================
+SCP
+;===================
+
+;--------------------
+Received from MCP
+;--------------------
+
+;--------------------
+Sent to MCP
+;--------------------
+
+;--------------------
+Received from Synthesis subprocess
+;--------------------
+
+;--------------------
+Sent to Synthesis subprocess
+;--------------------
+
+
+
+;===================
+Synthesis subprocess
+;===================
+
+;--------------------
+Received from SCP
+;--------------------
+(ping)
+(stop)
+(synthesize (,definitions ,inputs ,outputs) ,synthesis-id)
+
+;--------------------
+Sent to SCP
+;--------------------
+(ping)
+(stopped)
+;; error messages sent to SCP:
+(unexpected-eof)
+(unknown-message-type ,msg)
