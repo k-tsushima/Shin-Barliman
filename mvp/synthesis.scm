4d6120b580a61487a2d80283add5093634862089
William Byrd <webyrd@gmail.com>
2019-12-19 16:02:36 +0900

    updated sythesis code, and added test program for testing synthesis

--- mvp/synthesis.scm
+++ mvp/synthesis.scm
@@ -2,19 +2,21 @@
 (load "mk-chez.scm")
 (load "mk.scm")
 (load "interp.scm")
-(load "sythesis-template.scm")
+(load "synthesis-template.scm")
 
 (define FUEL 100000) ; ticks
 
 (define *engine-box* (box #f))
+(define *start-time* (box #f))
 
 (write '(synthesis-subprocess-ready))
+(flush-output-port)
 
 (let loop ((msg (read)))
   (cond
     ((eof-object? msg)
      (write `(unexpected-eof) (current-error-port))
-     (flush-output-port)
+     (flush-output-port (current-error-port))
      (exit))
     (else
      (pmatch msg
@@ -27,6 +29,7 @@
           (let ((e (make-engine (lambda ()
                                   (list (eval expr) synthesis-id)))))
             (set-box! *engine-box* e)
+            (set-box! *start-time* (current-time))
             (if (input-port-ready? (current-input-port))
                 (loop (read))
                 (loop `(no-message-to-read)))))]
@@ -42,7 +45,13 @@
                          `(expired ,e)))
               [(completed ,remaining-fuel ,val ,synthesis-id)
                (set-box! *engine-box* #f)
-               (write `(synthesis-finished ,synthesis-id ,val ,statistics))               
+               (let ((elapsed-time (time-difference (current-time) (unbox *start-time*))))
+                 (let ((elapsed-seconds (time-second elapsed-time))
+                       (elapsed-nanoseconds (time-nanosecond elapsed-time)))
+                   (let ((statistics `(elapsed-time (seconds ,elapsed-seconds) (nanoseconds ,elapsed-nanoseconds))))
+                     (write `(synthesis-finished ,synthesis-id ,val ,statistics)))))
+               (set-box! *start-time* #f)
+               (write `(synthesis-finished ,val))
                (flush-output-port)
                (loop (read))]
               [(expired ,e)
@@ -52,5 +61,5 @@
             (loop `(no-message-to-read)))]
        [,msg
         (write `(unknown-message-type ,msg) (current-error-port))
-        (flush-output-port)
+        (flush-output-port (current-error-port))
         (exit)]))))
