44403a9da9fd007d613ca963f27113ae04327210
William Byrd <webyrd@gmail.com>
2019-12-29 04:21:09 -0500

    changing MCP to use Chez rather than Racket

---
+++ mvp/mcp.scm
@@ -0,0 +1,18 @@
+;; Shin-Barliman Main Control Process (MCP)
+
+;; Uses separate TCP ports for UI and SCP connections.
+;;
+;; For example, UI might connect on port 8081, while
+;; SCP connects on port 8082.
+
+#|
+Description of the Main Control Process.
+----------------------------------------
+
+The MCP is responsible for coordinating communication between the
+user interface (UI) and the sub-processes responsible for synthesis.
+
+The MCP is also responsible for the policies and strategies used for
+efficient synthesis.
+|#
+
