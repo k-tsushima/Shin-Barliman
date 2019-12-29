75034f2d1ee5c2fc22346ecc62253a6f23cf4408
William Byrd <webyrd@gmail.com>
2019-12-29 14:38:17 -0500

    working on mcp proxies

--- mvp/mcp-ui-tcp-proxy.rkt
+++ mvp/mcp-ui-tcp-proxy.rkt
@@ -37,14 +37,46 @@
 
 (define (accept-and-handle listener)
   (define-values (in out) (tcp-accept listener))
+  (logf "mcp-ui-tcp-proxy accepted tcp connection")
   (handle in out)
+  (logf "mcp-ui-tcp-proxy closing tcp connection")
   (close-input-port in)
   (close-output-port out))
 
+(define (forward-from-mcp-to-ui out)
+  (lambda ()
+    (let loop ((msg (read)))
+      (logf "mcp-ui-tcp-proxy received message from mcp ~s\n" msg)
+      (cond
+        ((eof-object? msg)
+         ;; TODO
+         )
+        (else
+         ;; forward message to UI
+         (write msg out)
+         (flush-output-port out)
+         (logf "mcp-ui-tcp-proxy forwarded message to ui ~s\n" msg)
+         (loop (read)))))))
+
+(define (forward-from-ui-to-mcp in)
+  (let loop ((msg (read in)))
+    (logf "mcp-ui-tcp-proxy received message from ui ~s\n" msg)
+    (cond
+      ((eof-object? msg)
+       ;; TODO
+       )
+      (else
+       ;; forward message to MCP
+       (write msg)
+       (flush-output-port)
+       (logf "mcp-ui-tcp-proxy forwarded message to mcp ~s\n" msg)
+       (loop (read in))))))
+
 (define (handle in out)
-  ;; TODO
-  (void)
-  ;;
-  )
+  (logf "handle called for mcp-ui-tcp-proxy\n")
+  (define mcp-to-ui-thread (thread (forward-from-mcp-to-ui out)))
+  (forward-from-ui-to-mcp in)
+  ;; perhaps should use a custodian for thread cleanup
+  (kill-thread mcp-to-ui-thread))
 
 (serve DEFAULT-TCP-PORT)
