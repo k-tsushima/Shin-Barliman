e40b21f2317c1bcf34fc68b53cfff2e2b45d0326
William Byrd <webyrd@gmail.com>
2019-12-29 04:43:38 -0500

    working on MCP

---
+++ mvp/tcp-scp-proxy.rkt
@@ -0,0 +1,27 @@
+#lang racket/base
+
+; Proxy started by MCP for TCP communication with one or more SCPs
+
+(require
+  racket/tcp
+  "common.rkt")
+
+(provide  
+  (all-from-out racket/tcp)
+  (all-defined-out))
+
+
+(print-as-expression #f)
+
+;; Loading will occur at first use if not explicitly forced like this.
+(load-config #t)
+
+(define DEFAULT-TCP-IP-ADDRESS (config-ref 'scp-tcp-ip-address))
+(define DEFAULT-TCP-PORT (config-ref 'scp-tcp-port))
+
+;; TODO add code here
+
+(connect DEFAULT-TCP-IP-ADDRESS DEFAULT-TCP-PORT)
+
+;; > (require "tcp-scp-proxy.rkt")
+;; > (connect "localhost" 8080)
