448244934690dfd85d9a6b80d199553b3a161a5a
William Byrd <webyrd@gmail.com>
2019-12-31 12:50:30 -0500

    added logging infrastructure to scp-tcp-proxy

--- mvp/scp-tcp-proxy.rkt
+++ mvp/scp-tcp-proxy.rkt
@@ -19,6 +19,8 @@
 (define DEFAULT-TCP-IP-ADDRESS (config-ref 'scp-tcp-ip-address))
 (define DEFAULT-TCP-PORT (config-ref 'scp-tcp-port))
 
+(define PROGRAM-NAME "scp-tcp-proxy")
+
 ; (define *program* (box #f))
 ; (define *tests* (box #f))
 ; (define *scm-files* (box #f))
@@ -26,9 +28,34 @@
 (define *data* (box #f))
 (define *tcp-out* #f)
 
+#| begin logging infrastructure definitions (how to abstract this?) |#
+(define ENABLE-LOGGING (config-ref 'enable-proxy-logging))
+(define LOG-FILE-NAME (format "~a.log" PROGRAM-NAME))
+(define LOG-FILE-OUTPUT-PORT-BOX (box #f))
+
+;; semaphore code, to ensure logging is atomic, is adapted from
+;; https://docs.racket-lang.org/guide/concurrency.html?q=semaphore#%28part._.Semaphores%29
+(define log-output-semaphore (make-semaphore 1))
+
+(define (logf format-str . args)
+  (when ENABLE-LOGGING
+    (call-with-semaphore
+     log-output-semaphore
+     (lambda ()
+       (unless (unbox LOG-FILE-OUTPUT-PORT-BOX)
+         (define output-port (open-output-file LOG-FILE-NAME
+                                               #:mode 'text
+                                               #:exists 'replace))
+         (set-box! LOG-FILE-OUTPUT-PORT-BOX output-port))
+       (apply fprintf (unbox LOG-FILE-OUTPUT-PORT-BOX) format-str args)
+       (flush-output (unbox LOG-FILE-OUTPUT-PORT-BOX))))))
+#| end logging infrastructure definitions |#
+
+(logf "started ~a\n" PROGRAM-NAME)
+
 (define (handle-scp)
    (let loop ((msg (read (current-input-port))))
-     ; (printf "subprocess-client received message from SCP ~s\n" msg)
+     ; (printf "subprocess-client received message from SCP ~s\n" msg)     
      (write msg *tcp-out*)
      (flush-output *tcp-out*)
      ; (printf "subprocess-client sent message from MCP ~s\n" msg)
