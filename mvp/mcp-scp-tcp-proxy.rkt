2383ac4dfaae16a616d76c5ed01dff4b265987c4
William Byrd <webyrd@gmail.com>
2020-01-04 22:35:43 -0600

    added newling to log message

--- mvp/mcp-scp-tcp-proxy.rkt
+++ mvp/mcp-scp-tcp-proxy.rkt
@@ -173,7 +173,7 @@
       (set! *scp-connections*
             (cons `(,scp-id ,scp-in ,scp-out)
                   *scp-connections*))
-      (logf "Updated *scp-connections* table: ~s" *scp-connections*)))
+      (logf "Updated *scp-connections* table: ~s\n" *scp-connections*)))
   (write `(scp-id ,scp-id) scp-out)
   (flush-output scp-out)
   (logf "mcp-to-scp handshake established\n"))
