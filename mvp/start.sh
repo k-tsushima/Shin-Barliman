908b40ac4096d85c66a4e3aa56284dbc3865a2aa
William Byrd <webyrd@gmail.com>
2020-01-19 11:28:11 -0600

    start.sh script now starts two SCPs instead of one

--- mvp/start.sh
+++ mvp/start.sh
@@ -5,3 +5,4 @@ rm -f *.log
 osascript -e "tell application \"terminal\" to do script \"cd ${PWD}; racket gui.rkt\"" &
 osascript -e "tell application \"terminal\" to do script \"cd ${PWD}; scheme mcp.scm\"" &
 osascript -e "tell application \"terminal\" to do script \"cd ${PWD}; sleep 3; scheme scp.scm\"" &
+osascript -e "tell application \"terminal\" to do script \"cd ${PWD}; sleep 3; scheme scp.scm\"" &
